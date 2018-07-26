71af6906229dbf2aea7fa2f25ed3abff2b73c159
cyril jardin <cyilroc60@hotmail.fr>
2018-07-26 15:28:55 +0200

    variable

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8"/>
+    <title>exo8</title>
+  </head>
+  <body>
+    <p>
+    <?php
+    $one= 3 + 4;
+    $two= 5 * 20;
+    $three= 45 / 5;
+    echo "Mon premier est $one, mon second est $two et mon dernier est $three.";
+     ?>
+   </p>
+  </body>
+</html>
